3727e51251c7712164fa43e6b98361a3cfdf4552
Keanna Salter <keannasalter91@gmail.com>
2020-02-05 20:34:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,17 @@
 class Dog
   def name=(dogs_name)
-    name = dogs_name
+    name=dogs_name
   end
 end
 
 def name
-  @name
+  name
 end
 
 class Dog
   def bark
     puts "woof"
   end
+
 end
   
